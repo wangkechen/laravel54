84bfc96801d361843d2386503e95d92a61ca927c
wangkechen <wangkechen@ecosysnet.com>
2017-03-07 19:45:03 +0800

    episode 18

--- app/Post.php
+++ app/Post.php
@@ -22,6 +22,11 @@ class Post extends Model
         return $this->hasMany(Comment::class);
     }
 
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
     public function addComment($body)
     {
         /*
